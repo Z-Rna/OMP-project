5743fb92be6a956f4a7678110a70e5540189c8f6
Z-Rna <wadczyniciastek@gmail.com>
2021-04-28 20:19:29 +0200

    change table

--- primes/domain.cpp
+++ primes/domain.cpp
@@ -6,6 +6,34 @@ std::vector<int> domain::primes(int start, int end, bool printOutput)
     int size = end - start + 1;
     std::vector<int> result;
     double timeStart, timeStop;
+    int threadsCount = omp_get_max_threads();
+
+    result.reserve(size);
+    timeStart = omp_get_wtime();
+
+    std::vector<std::vector<int>> privateResults(threadsCount, std::vector<int>());
+    for (int i = 0; i < threadsCount; i++) privateResults[i].reserve(end - start + 1);
+#pragma omp parallel
+    {
+#pragma omp for
+        for (int i = start; i <= end; i++)
+        {
+            if (isPrime(i)) privateResults[omp_get_thread_num()].push_back(i);
+        }
+    }
+    for (size_t i = 0; i < threadsCount; i++) result.insert(result.end(), privateResults[i].begin(), privateResults[i].end());
+
+    timeStop = omp_get_wtime();
+    if (printOutput) period(timeStart, timeStop, "funcional::primes");
+
+    return result;
+}
+
+std::vector<int> domain::primesNaive(int start, int end, bool printOutput)
+{
+    int size = end - start + 1;
+    std::vector<int> result;
+    double timeStart, timeStop;
 
     result.reserve(size);
     timeStart = omp_get_wtime();
--- primes/domain.h
+++ primes/domain.h
@@ -4,6 +4,7 @@
 namespace domain {
 	static int IT;
 	std::vector<int> primes(int start, int end, bool printOutput = true);
+	std::vector<int> primesNaive(int start, int end, bool printOutput = true);
 	std::vector<int> basicSieve(int start, int end, bool printOutput = true);
 	std::vector<int> optimizedSive(int start, int end, bool printOutput = true);
 	int* optimizedSiveDynamic(int start, int end, bool printOutput = true);
